a0b5de5f9efd9c80c09f9185499c7d59352450e5
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-11-13 21:44:59 +0900

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,3 @@
-# docker build -t proof-test .
-# docker run --entrypoint=sh -ti ebproof-test
-# exit
-# docker cp container_name:app/proof1.png .
-
-# ref: https://tex.stackexchange.com/questions/268997
-
-# latex -halt-on-error -interaction=nonstopmode -output-directory out ebproof.tex
-# pdflatex -output-directory out ebproof.tex
-# dvipng -D 600 -o out/ebproof.png out/ebproof.dvi
-# dvisvgm --no-fonts --bbox=preview -o out/ebproof.svg out/ebproof.dvi
-
-# latex -halt-on-error -interaction=nonstopmode -output-directory out forest.tex
-# dvisvgm --no-fonts --bbox=preview -o out/forest.svg out/forest.dvi
-
 FROM alpine:latest AS installer
 RUN apk add --no-cache perl tar wget
 WORKDIR /install-tl-unx
---
+++ test.sh
@@ -0,0 +1,16 @@
+# docker build -t proof-test .
+
+latex -halt-on-error -interaction=nonstopmode -output-directory out examples/bussproofs.tex
+pdflatex -halt-on-error -interaction=nonstopmode -output-directory out examples/bussproofs.tex
+dvipng -D 600 -o out/bussproofs.png out/bussproofs.dvi
+dvisvgm --no-fonts --bbox=preview -o out/bussproofs.svg out/bussproofs.dvi
+
+latex -halt-on-error -interaction=nonstopmode -output-directory out examples/ebproof.tex
+pdflatex -halt-on-error -interaction=nonstopmode -output-directory out examples/ebproof.tex
+dvipng -D 600 -o out/ebproof.png out/ebproof.dvi
+dvisvgm --no-fonts --bbox=preview -o out/ebproof.svg out/ebproof.dvi
+
+latex -halt-on-error -interaction=nonstopmode -output-directory out examples/forest.tex
+pdflatex -halt-on-error -interaction=nonstopmode -output-directory out examples/forest.tex
+dvipng -D 600 -o out/forest.png out/forest.dvi
+dvisvgm --no-fonts --bbox=preview -o out/forest.svg out/forest.dvi
