9c0fae9029c2ba332bb79e86d55efc5262624734
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-16 21:19:44 +0900

    wip

--- main.sh
+++ main.sh
@@ -6,8 +6,8 @@ for texfile in in/*.tex; do
     dvisvgm --bbox=preview --bitmap-format=none --font-format=woff2 --optimize --relative -o "out/${name}.svg" "out/${name}.dvi"
     pdflatex -halt-on-error -interaction=nonstopmode -output-directory out "$texfile"
     gs -dBATCH -dNOPAUSE -r600 -sDEVICE=pngmono -o "out/${name}.png" "out/${name}.pdf"
-    gs -dBATCH -dCompatibilityLevel=1.5 -dNOPAUSE -sDEVICE=pdfwrite -o "out/${name}_comp.pdf" "out/${name}.pdf"
-    mv "out/${name}_comp.pdf" "out/${name}.pdf"
+    gs -dBATCH -dCompatibilityLevel=1.5 -dNOPAUSE -sDEVICE=pdfwrite -o "out/${name}-comp.pdf" "out/${name}.pdf"
+    mv "out/${name}-comp.pdf" "out/${name}.pdf"
 done
 
 rm out/*.aux out/*.dvi out/*.log
